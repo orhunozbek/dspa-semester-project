8ef78d5d4afe7cfe8d27ce9d14df9f032742e2e7
Patrick Stäuble <patricst@student.ethz.ch>
2019-03-31 13:29:16 +0200

    Refactor Project structure, added tests

---
+++ src/main/java/test/JUnitTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JUnitTest {
+
+    @Test
+    void addition() {
+        assertEquals(2, 2);
+    }
+}
