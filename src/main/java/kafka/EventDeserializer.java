d9b3362847c9cafb8c1ec3af5b5712064e51ea45
orhunozbek <oezbeko@student.ethz.edu>
2019-04-05 00:28:33 +0200

    Since topicName is added to the record, it causes problem while deserialization, Therefore added a feature to ignore unknown properties

--- src/main/java/kafka/EventDeserializer.java
+++ src/main/java/kafka/EventDeserializer.java
@@ -1,5 +1,6 @@
 package kafka;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
@@ -11,13 +12,13 @@ public class EventDeserializer<T> implements
         DeserializationSchema<T> {
 
     private Class<T> eventClass;
+    private static ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule());
 
     public EventDeserializer(Class<T> eventClass) {
         this.eventClass = eventClass;
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
-
-    private static ObjectMapper objectMapper = new ObjectMapper()
-            .registerModule(new JavaTimeModule());
 
     @Override
     public T deserialize(byte[] bytes) throws IOException {
