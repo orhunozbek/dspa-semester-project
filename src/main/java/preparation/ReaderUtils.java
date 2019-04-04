7dbf6cefa5360c36d4a1b95b3b4d7bad5f4f219b
orhunozbek <oezbeko@student.ethz.edu>
2019-04-04 23:07:35 +0200

    Added ReaderUtils class to parse csv files easier and avoid code duplication as much as possible

---
+++ src/main/java/preparation/ReaderUtils.java
@@ -0,0 +1,94 @@
+package preparation;
+
+import main.Main;
+import org.apache.commons.configuration2.Configuration;
+
+import java.io.File;
+
+
+public class ReaderUtils {
+
+    public enum Directory {
+        InputDirectory {
+            public String toString() {
+                return "inputDirectory";
+            }
+        },
+
+        WorkingDirectory {
+            public String toString() {
+                return "workingDirectory";
+            }
+        }
+    }
+
+    public enum Topic {
+        Like {
+            public String toString() {
+                return "/streams/likes_event_stream.csv";
+            }
+        },
+
+        Comment {
+            public String toString() {
+                return "/streams/comment_event_stream.csv";
+            }
+        },
+
+        Post {
+            public String toString() {
+                return "/streams/post_event_stream.csv";
+            }
+        }
+
+    }
+
+    public static File getFile(Directory dirName, Topic topicName) {
+
+        Configuration configuration = Main.getGlobalConfig();
+        if (configuration == null) {
+            return null;
+        }
+        String dataDirectory = configuration.getString(dirName.toString());
+        File eventStreamFile = new File(dataDirectory + topicName.toString());
+        if (!eventStreamFile.exists()) {
+            return null;
+        }
+
+        return eventStreamFile;
+    }
+
+    public static String[] getHeaderFor(Directory dirName, Topic topic) {
+
+        if (dirName == Directory.WorkingDirectory)
+        {
+        switch (topic) {
+            case Comment:
+                return new String[]{"timeMilisecond", "id", "personId", "creationDate", "locationIP",
+                        "browserUsed", "content", "reply_to_postId", "reply_to_commentId", "placeId"};
+            case Like:
+                return new String[]{"timeMilisecond", "Person.id", "Post.id", "creationDate"};
+            case Post:
+                return new String[]{"timeMilisecond", "id", "personId", "creationDate", "imageFile",
+                        "locationIP", "browserUsed", "language", "content", "tags", "forumId", "placeId"};
+            default:
+                return null;
+        }}
+        else if (dirName == Directory.InputDirectory){
+            switch (topic) {
+                case Comment:
+                    return new String[]{"id", "personId", "creationDate", "locationIP",
+                            "browserUsed", "content", "reply_to_postId", "reply_to_commentId", "placeId"};
+                case Like:
+                    return new String[]{"Person.id", "Post.id", "creationDate"};
+                case Post:
+                    return new String[]{"id", "personId", "creationDate", "imageFile",
+                            "locationIP", "browserUsed", "language", "content", "tags", "forumId", "placeId"};
+                default:
+                    return null;
+            }
+        }
+        else return null;
+    }
+
+}
