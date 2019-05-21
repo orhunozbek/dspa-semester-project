08ee845ac02814eac12f8a2a227e110a7601dedd
orhunozbek <oezbeko@student.ethz.edu>
2019-05-21 14:02:13 +0200

    Deleted unused import from TupleSerializationSchema

--- src/main/java/kafka/TupleSerializationSchema.java
+++ src/main/java/kafka/TupleSerializationSchema.java
@@ -2,7 +2,6 @@ package kafka;
 
 import org.apache.flink.api.common.serialization.SerializationSchema;
 import org.apache.flink.api.java.tuple.Tuple;
-import org.apache.flink.api.java.tuple.Tuple4;
 
 public class TupleSerializationSchema<T extends Tuple> implements SerializationSchema<T> {
     @Override
